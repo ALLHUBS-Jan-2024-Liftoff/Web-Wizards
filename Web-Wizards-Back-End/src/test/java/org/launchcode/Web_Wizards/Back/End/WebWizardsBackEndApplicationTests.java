7e9ada80211aac401389834b43d73aefb7f06f4e
Zachary L. Cain <120860641+cain9209@users.noreply.github.com>
2024-07-08 20:50:20 -0500

    Add files via upload
    Back end addition

---
+++ Web-Wizards-Back-End/src/test/java/org/launchcode/Web_Wizards/Back/End/WebWizardsBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package org.launchcode.Web_Wizards.Back.End;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebWizardsBackEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
