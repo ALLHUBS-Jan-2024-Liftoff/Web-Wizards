a4dc1d2a3a9d69752ada83c48ed9e03bc320749d
cain9209 <zacharylcain@gmail.com>
2024-07-11 22:04:54 -0500

    Working Back end login information to fill in missing structure.

--- Show-MO-events-BackEnd/src/main/java/org/launchcode/BackEnd/models/AbstractEntity.java
+++ Show-MO-events-BackEnd/src/main/java/org/launchcode/BackEnd/models/AbstractEntity.java
@@ -1,2 +1,34 @@
-package org.launchcode.BackEnd.models;public class AbstractEntity {
+package org.launchcode.BackEnd.models;
+
+
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.MappedSuperclass;
+
+import java.util.Objects;
+
+@MappedSuperclass
+public abstract class AbstractEntity {
+
+    @Id
+    @GeneratedValue
+    private int id;
+
+    public int getId() {
+        return id;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        AbstractEntity that = (AbstractEntity) o;
+        return id == that.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
 }
