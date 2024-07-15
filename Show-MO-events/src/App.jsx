5b2a1214b2fcea3ffaf63f4008aa02f398ac9666
cain9209 <zacharylcain@gmail.com>
2024-07-15 14:55:03 -0500

    Landing page information template.

--- Show-MO-events/src/App.jsx
+++ Show-MO-events/src/App.jsx
@@ -1,11 +1,17 @@
 import React from 'react';
-import Header from './components/Header'; // Adjust the import path as per your project structure
-import './Navbar.css'; // Make sure to adjust the CSS import if needed
+import Header from './components/Navbar'; // Adjust the import path as per your project structure
+import './css/Navbar.css'; // Make sure to adjust the CSS import if needed
+import './css/LoginForm.css'; // Make sure the path is correct
+import LoginForm from './components/LoginForm'; // Corrected import
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <div>
+        <h1>Login Here!</h1>
+        <LoginForm />
+      </div>
     </div>
   );
 }
--- Show-MO-events/src/components/Navbar.jsx
+++ Show-MO-events/src/components/Navbar.jsx
@@ -11,11 +11,9 @@ const Navbar = () => {
                 </a>
 
       <ul className="nav-links">
-        <li><a href="/">Home</a></li>
         <li><a href="/about">About</a></li>
         <li><a href="/services">Services</a></li>
         <li><a href="/contact">Contact</a></li>
-        <li><a href="/contact">Login/Register</a></li>
       </ul>
     </div>
 
