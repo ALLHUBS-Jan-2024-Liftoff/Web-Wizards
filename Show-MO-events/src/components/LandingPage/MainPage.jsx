7e5a5aaa69c9516e91995cac7250937f8f154c8a
Zachary L. Cain <120860641+cain9209@users.noreply.github.com>
2024-07-15 21:38:40 -0500

    fix idea and .gitignore (#47)
    * Working front end demo
    
    * Landing page information template.
    
    * Landing page information template.
    
    * Landing page information template.
    
    * Landing page information template.
    
    * Landing page information template.
    
    ---------
    
    Co-authored-by: Aishwarya Senthilkumar <124016737+AishuSenthil@users.noreply.github.com>

---
+++ Show-MO-events/src/components/LandingPage/MainPage.jsx
@@ -0,0 +1,15 @@
+import Navbar from './Navbar';
+import LoginForm from './LoginForm';
+import SlideShow from './SlideShow';
+
+const MainPage = () => {
+    return (
+        <div>
+            <Navbar />
+            <LoginForm />
+            <SlideShow />
+        </div>
+    );
+}
+
+export default MainPage;
