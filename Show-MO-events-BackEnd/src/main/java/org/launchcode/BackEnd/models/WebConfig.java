935fd2a81a0e6cf7b9b0f8f2a84d5e7a6a02e0e4
Zachary L. Cain <120860641+cain9209@users.noreply.github.com>
2024-08-08 20:52:05 -0500

    Update to Web config (#67)

--- Show-MO-events-BackEnd/src/main/java/org/launchcode/BackEnd/models/WebConfig.java
+++ Show-MO-events-BackEnd/src/main/java/org/launchcode/BackEnd/models/WebConfig.java
@@ -10,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/api/**")
+        registry.addMapping("/**")
                 .allowedOrigins("http://localhost:5173") // Allow frontend origin
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // Allow specific methods
                 .allowedHeaders("*")
